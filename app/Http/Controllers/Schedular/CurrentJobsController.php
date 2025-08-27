9e170a8d226977d006b8c123ccd610d6114efedf
thisissyedsaad <syedsaaduzzubair@gmail.com>
2025-08-27 18:41:03 +0500

    add login in midpoint check count

--- app/Http/Controllers/Schedular/CurrentJobsController.php
+++ app/Http/Controllers/Schedular/CurrentJobsController.php
@@ -534,12 +534,17 @@ class CurrentJobsController extends Controller
             $collectionTime = Carbon::createFromFormat('Y-m-d H:i', $pickup['date'] . ' ' . $pickup['toTime']);
             $deliveryTime = Carbon::createFromFormat('Y-m-d H:i', $delivery['date'] . ' ' . $delivery['deliveryTime']);
             $tracking = CurrentJobsTracking::where('order_id', $orderId)->first();
-            
+            $currentTime = Carbon::now();
+
             if ($deliveryTime->diffInHours($collectionTime) >= 2) {
                 // $midpointTime = $collectionTime->copy()->addMinutes($deliveryTime->diffInMinutes($collectionTime) / 2);
-                $midpointTime = $collectionTime->copy()->addMinutes($deliveryTime->diffInMinutes($collectionTime) / 2)->format('H:i');
-                if (!$tracking || !$tracking->midpoint_check_completed) {
-                    return $midpointTime;
+                $midpointTime = $collectionTime->copy()->addMinutes($deliveryTime->diffInMinutes($collectionTime) / 2);
+                // if (!$tracking || !$tracking->midpoint_check_completed) {
+                //     return $midpointTime;
+                // }
+                if ($currentTime->greaterThan($midpointTime) && 
+                    (!$tracking || !$tracking->midpoint_check_completed)) {
+                    return $midpointTime->format('H:i');
                 }
             }
             return false;
